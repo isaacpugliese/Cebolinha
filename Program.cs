e25e6073a259be5c189acef5a9c8e525492ee4fd
isaac pugliese <isaacpugliese2010@gmail.com>
2020-09-20 16:08:56 -0300

    cebolinha

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace cebolinha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite uma frase para Falar Igual o Cebolinha: ");
+            string Frase = Console.ReadLine();
+            Console.WriteLine(Frase.Replace("r" , "l"));
+
+        }
+    }
+}
